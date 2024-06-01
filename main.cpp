2730cf2b9a369460d4fc03088712784265e67c8b
Nikita Krenov <nikitabaram@gmail.com>
2024-06-01 20:54:43 +0300

    sum arr with arr

--- main.cpp
+++ main.cpp
@@ -3,7 +3,20 @@
 using namespace std;
 
 int main() {
-	
+	const int size = 5;
+    int arr1[size] = { 1, 2, 3, 4, 5 };
+    int arr2[size] = { 6, 7, 8, 9, 10 };
+    int result[size];
+
+    for (int i = 0; i < size; ++i) {
+        result[i] = arr1[i] + arr2[i];
+    }
+
+    std::cout << "Result sum arr: ";
+    for (int i = 0; i < size; ++i) {
+        std::cout << result[i] << " ";
+    }
+    std::cout << std::endl;
 
 	system("pause");
 	return 0;
